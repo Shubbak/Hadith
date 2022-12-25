6ffca59048ee332af30fd4bead940e4e6d47e81b
HarunMerdic <merdicharun@gmail.com>
2022-12-25 19:16:56 +0100

    Arabic extract from Website

--- python/Extract_englishHadith_website.py
+++ python/Extract_englishHadith_website.py
@@ -11,22 +11,25 @@ Created on Sun Dec 25 17:23:29 2022
 @author: merdi
 """
 #import numpy as np
-
 import requests
 from bs4 import BeautifulSoup
 
 hadis = []
 url = 'https://quranx.com/hadith/Bukhari/DarusSalam/Hadith-'
-n_pages = 20
+n_pages = 3
 
-with open("test_Website_arabisch_extract.txt", 'w', encoding='utf-8') as output:
+with open("Website_english_extract.txt", 'w', encoding='utf-8') as output:
     for i in range (1,n_pages+1):
         url_site = url+str(i)
         res = requests.get(url_site)
         html_page = res.content
-        soup = BeautifulSoup(html_page, 'html.parser')
-        result = soup.find_all("div", class_="hadith__text arabic")
-        # text= "Dar-us-Salam reference Hadith"+str(i)
+       # soup = BeautifulSoup(html_page, 'html.parser')
+       # result = soup.find_all("div", class_="hadith__text")
+       result=soup.findAll('div') 
+#to get the value inside of span you could do:
+
+soup.find('hadith__text').get_text()
+       
         
         for res in result:
                 hadis = res.text
