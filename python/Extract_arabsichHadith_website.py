57dd3a0e1c2f2d3e9a24dc284670282aaa0627e1
Shubbak <a.shubbak@outlook.com>
2022-12-26 17:42:14 +0100

    I tried to fix a bug where random ahadith were omitted
    It seems like the problem is with the website and it's overloaded.
    That's why I put a 1 sec pause between every instance of the for-loop.
    This improved the situation a lot but not 100%.
    Website_arabisch_extract.txt ist the last file with 7291 empty lines ->
    7291 or 7290 Ahadith.
    Bukhari1-3. are the old .txt.

--- python/Extract_arabsichHadith_website.py
+++ python/Extract_arabsichHadith_website.py
@@ -7,21 +7,27 @@ Created on Sun Dec 25 17:23:29 2022
 import numpy as np
 import requests
 from bs4 import BeautifulSoup
+import time
 
-hadis = []
+list_of_hadis = []
 url = 'https://quranx.com/hadith/Bukhari/DarusSalam/Hadith-'
+start_page = 1
 n_pages = 7563
 
-with open("Website_arabisch_extract.txt", 'w', encoding='utf-8') as output:
-    for i in range (1,n_pages+1):
+with open("Website_arabisch_extract.txt", 'a', encoding='utf-8') as output:
+    for i in range (start_page,n_pages+1):
         url_site = url+str(i)
         res = requests.get(url_site)
         html_page = res.content
         soup = BeautifulSoup(html_page, 'html.parser')
         result = soup.find_all("div", class_="hadith__text arabic")
         # text= "Dar-us-Salam reference Hadith"+str(i)
+        list_of_hadis.append(result)
         
         for res in result:
-                hadis = res.text
+                hadis = str(i) + res.text
                 
                 print( hadis , file=output)
+        time.sleep(2)
+
+    print("you appended {} ahadith.".format(len(list_of_hadis)), file=output)
